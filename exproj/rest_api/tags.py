199a02f9384942be6dd70fc06630193271030a06
Mark Podkolzin <mpodkolzin@miem.hse.ru>
2020-04-18 21:35:31 +0300

    Add validation to tag name

--- exproj/rest_api/tags.py
+++ exproj/rest_api/tags.py
@@ -3,6 +3,7 @@ from flask_login import login_required
 
 from . import make_ok, access_required
 from exproj.logic import tags as tags_logic
+from exproj.validation import validate_tag_name
 
 bp = Blueprint('tags', __name__, url_prefix='/tag')
 
@@ -18,6 +19,7 @@ def get_tags():
 @access_required('moderator')
 def create_tag():
     name = request.args.get('name')
+    validate_tag_name(name)
     tags_logic.create(name)
     return make_ok('Tag successfully created'), 201
 
@@ -27,6 +29,7 @@ def create_tag():
 @access_required('moderator')
 def update_tag(tag):
     new_name = request.args.get('name')
+    validate_tag_name(new_name)
     tags_logic.update(tag, new_name)
     return make_ok('Tag successfully updated')
 
--- exproj/validation/__init__.py
+++ exproj/validation/__init__.py
@@ -3,6 +3,13 @@ from flask import abort
 from exproj.db import get_session, Tag
 
 
+def validate_tag_name(name):
+    if (not name
+            or '/' in name
+            or '?' in name):
+        abort(422, 'Tag must not be empty or contain `?` or `/`')
+
+
 def validate_tags(tag_names):
     with get_session() as s:
         tags = s.query(Tag).filter(Tag.name.in_(tag_names)).all()
